67adad33d631c99f2acd6ff2dc47a73a6dcb7410
Javascript <javascript@zPro3.local>
2017-11-28 20:37:45 +0100

    base

--- Alimentos_v3/js/scripts.js
+++ Alimentos_v3/js/scripts.js
@@ -121,10 +121,15 @@ $(function() {
 			}
 		);
 	// Start the live stream scanner when the modal opens
-	$('#open-camera').on('click', function (e) {
+	$('#livestream_scanner').on('shown.bs.modal', function (e) {
 		Quagga.init(
 			liveStreamConfig, 
 			function(err) {
+				if (err) {
+					$('#livestream_scanner .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> '+err.name+'</strong>: '+err.message+'</div>');
+					Quagga.stop();
+					return;
+				}
 				Quagga.start();
 			}
 		);
@@ -162,14 +167,21 @@ $(function() {
 	Quagga.onDetected(function(result) {    		
 		if (result.codeResult.code){
 			$('#scanner_input').val(result.codeResult.code);
-			Quagga.stop(); 		
+			Quagga.stop();	
+			setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);			
 		}
 	});
     
+	// Stop quagga in any case, when the modal is closed
+    $('#livestream_scanner').on('hide.bs.modal', function(){
+    	if (Quagga){
+    		Quagga.stop();	
+    	}
+    });
 	
 	// Call Quagga.decodeSingle() for every file selected in the 
 	// file input
-	$("#open-camera input:file").on("change", function(e) {
+	$("#livestream_scanner input:file").on("change", function(e) {
 		if (e.target.files && e.target.files.length) {
 			Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {alert(result.codeResult.code);});
 		}
