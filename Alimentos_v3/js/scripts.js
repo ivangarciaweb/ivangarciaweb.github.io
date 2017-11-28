d2ee91d00fb996c74114d8608cc5db1a0562c5cb
Javascript <javascript@zPro3.local>
2017-11-28 20:27:40 +0100

    prueba nuevo ajuste js

--- Alimentos_v3/js/scripts.js
+++ Alimentos_v3/js/scripts.js
@@ -103,18 +103,10 @@ $(function() {
 			},
 			numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
 			decoder: {
-				readers: [
-					"code_128_reader",
-					"ean_reader",
-					"ean_8_reader",
-					"code_39_reader",
-					"code_39_vin_reader",
-					"codabar_reader",
-					"upc_reader",
-					"upc_e_reader",
-					"i2of5_reader"
+				"readers":[
+					{"format":"ean_reader","config":{}}
 				]
-			}
+			},
 			locate: true
 		};
 	// The fallback to the file API requires a different inputStream option. 
@@ -140,29 +132,29 @@ $(function() {
 	
 	// Make sure, QuaggaJS draws frames an lines around possible 
 	// barcodes on the live stream
-	// Quagga.onProcessed(function(result) {
-	// 	var drawingCtx = Quagga.canvas.ctx.overlay,
-	// 		drawingCanvas = Quagga.canvas.dom.overlay;
+	Quagga.onProcessed(function(result) {
+		var drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
  
-	// 	if (result) {
-	// 		if (result.boxes) {
-	// 			drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-	// 			result.boxes.filter(function (box) {
-	// 				return box !== result.box;
-	// 			}).forEach(function (box) {
-	// 				Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-	// 			});
-	// 		}
+		if (result) {
+			if (result.boxes) {
+				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+				result.boxes.filter(function (box) {
+					return box !== result.box;
+				}).forEach(function (box) {
+					Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+				});
+			}
  
-	// 		if (result.box) {
-	// 			Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-	// 		}
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+			}
  
-	// 		if (result.codeResult && result.codeResult.code) {
-	// 			Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-	// 		}
-	// 	}
-	// });
+			if (result.codeResult && result.codeResult.code) {
+				Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+			}
+		}
+	});
 	
 	// Once a barcode had been read successfully, stop quagga and 
 	// close the modal after a second to let the user notice where 
@@ -174,6 +166,13 @@ $(function() {
 		}
 	});
     
+	// Stop quagga in any case, when the modal is closed
+    $('#open-camera').on('click, function(){
+    	if (Quagga){
+    		Quagga.stop();	
+    	}
+    });
+	
 	// Call Quagga.decodeSingle() for every file selected in the 
 	// file input
 	$("#open-camera input:file").on("change", function(e) {
